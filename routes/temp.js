4868501eac1d19d7a5c39dc1574f851014c8ea52
= <=>
2019-08-12 17:41:02 +0700

    temp update, delete in swagger

--- routes/temp.js
+++ routes/temp.js
@@ -44,7 +44,52 @@ router.post('/temp/add',(req,res,next) => {
             });
         }
     });
-})
+});
+
+router.post('/temp/update',(req,res,next) => {
+    let { value,id } = req.body;
+    let updateTempQuery =  `UPDATE temps SET value=${value} WHERE id=${id}`;
+    connection.query(updateTempQuery,(err,result,fields) => {
+        if(err){
+            res.status(500).json({
+                header: status500(err),
+                body: {
+                   'result': null
+                }
+            });
+        }else{
+            res.status(200).json({
+                header: status200,
+                body: {
+                    'result': result
+                }
+            });
+        }
+    });
+});
+
+router.post('/temp/delete/:id',(req,res,next) => {
+    let { id } = req.params;
+    let deleteTempQuery =  `DELETE FROM temps WHERE id=${id}`;
+    connection.query(deleteTempQuery,(err,result,fields) => {
+        if(err){
+            res.status(500).json({
+                header: status500(err),
+                body: {
+                   'result': null
+                }
+            });
+        }else{
+            res.status(200).json({
+                header: status200,
+                body: {
+                    'result': result
+                }
+            });
+        }
+    });
+});
+
 
 
 
